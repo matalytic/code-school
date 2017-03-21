4aecdf8451a23c11828cace5f149dd6a6bf9ec60
matalytic <matthewaarongo@gmail.com>
2017-03-21 13:38:03 -0400

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,12 @@
+alert();
+console.log();
+
+
+
+
+var whale = {
+	size: big;
+	color: blue;
+}
+
+var humpback = Object.create(whale);
---
+++ objectPractice.js
@@ -0,0 +1,11 @@
+// object practice
+
+
+var couch = {
+	color: "red",
+	size: "big".
+	legs: 4,
+	changeToBed = function () {
+		couch.size = "bigger"
+	}
+};
